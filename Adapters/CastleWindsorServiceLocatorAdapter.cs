a27e5fc0a603c94b95df80da769ea0a106a1fd2c
Tavi Truman <TaviTruman@users.noreply.github.com>
2018-07-07 06:27:34 -0700

    Initial upload and commit

---
+++ Adapters/CastleWindsorServiceLocatorAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Castle.Windsor;
+using IKW.Contropolus.ServiceLocator.CastleWindsorAdapter.Extensions;
+using Microsoft.Practices.ServiceLocation;
+
+namespace IKW.Contropolus.ServiceLocator.CastleWindsorAdapter.Adapters
+{
+    public class CastleWindsorServiceLocatorAdapter : ServiceLocatorImplBase
+    {
+        private readonly IWindsorContainer _windsorContainer;
+
+        public CastleWindsorServiceLocatorAdapter(IWindsorContainer theWindsorContainer)
+        {
+            _windsorContainer = theWindsorContainer;
+        }
+
+        public IWindsorContainer WindsorContainer => _windsorContainer;
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            return CastleWindsorContainerExtensions.Resolve(WindsorContainer, serviceType);
+
+            //if (key == null)
+            //    return WindsorContainer.Resolve(serviceType);
+            //return WindsorContainer.Resolve(key, serviceType);
+        }
+
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType) => (IEnumerable<object>) WindsorContainer.ResolveAll(serviceType);
+    }
+}
+
